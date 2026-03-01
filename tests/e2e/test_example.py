2584e996fa47f5290aab61cb4e7ba88725685ac5
Malik Mlitat <malik.mlitat@gmail.com>
2026-03-01 14:05:00 +0200

    feat: add goto fixture

--- tests/e2e/test_example.py
+++ tests/e2e/test_example.py
@@ -1,23 +1,8 @@
 import re
 from playwright.sync_api import Page, expect
 
-def test_has_title(page: Page):
-    page.goto("https://playwright.dev/")
-
-    # Expect a title "to contain" a substring.
-    expect(page).to_have_title(re.compile("Playwright"))
-
-def test_get_started_link(page: Page):
-    page.goto("https://playwright.dev/")
-
-    # Click the get started link.
-    page.get_by_role("link", name="Get started").click()
-
-    # Expects page to have a heading with the name of Installation.
-    expect(page.get_by_role("heading", name="Installation")).to_be_visible()
-
 def test_login_to_orangehrm(page: Page):
-    page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+
     page.get_by_placeholder("Username").fill("Admin")
     page.get_by_placeholder("Password").fill("admin123")
 
