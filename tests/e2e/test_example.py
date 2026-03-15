e34577c6d4774b817293ac578105e6be508e726f
Malik Mlitat <malik.mlitat@gmail.com>
2026-03-15 12:59:50 +0200

    feat: add LoginPage add valid/invalid login tests

--- tests/e2e/test_example.py
+++ tests/e2e/test_example.py
@@ -1,10 +1,26 @@
 import re
+
+import pytest
 from playwright.sync_api import Page, expect
 
-def test_login_to_orangehrm(page: Page):
+from pages.login_page import LoginPage
 
-    page.get_by_placeholder("Username").fill("Admin")
-    page.get_by_placeholder("Password").fill("admin123")
 
-    page.get_by_role("button", name="Login").click()
-    expect(page).to_have_title(re.compile("OrangeHRM"))
+def test_valid_login_to_orangehrm(page: Page):
+    login_page = LoginPage(page)
+    login_page.login_with_valid_admin()
+
+
+
+
+@pytest.mark.parametrize("username, password",
+                         [("admin", "0000"), # wrong passwrod
+                          ("peter", "admin123"), # wrong username
+                          ("jjfdf", "iohrfsdf"), # both
+                          ("مالك", "iohrfsdf") # both
+                          ]
+                         )
+def test_invalid_creds(page: Page, username, password):
+    login_page = LoginPage(page)
+    login_page.invalid_login(username=username, password=password)
+
